1f2a6730d2ec932a158381f3a46ec206e44fc66e
valentin <valentinl@bamlab.fr>
2016-07-26 10:34:24 +0200

    add webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,16 @@ module.exports = {
     output: {
         path: './bin',
         filename: 'bookomatic.bundle.js'
-    }
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+        }]
+    },
+    devServer: {
+        contentBase: "./bin",
+        hot: true
+    },
 };
